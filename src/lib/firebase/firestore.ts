7e0ce3e93df737534353200400a9b153581e8f49
AlphaVerse <eymencoban09@gmail.com>
2025-07-12 21:50:17 +0000

    Implemented v1.1 features. Fixed a few bugs

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -28,7 +28,7 @@ export const ADMIN_MAX_SNIPPETS = 75; // Define the admin limit
 
 // --- Admin Configuration ---
 // Add your Firebase User IDs here to grant admin privileges.
-export const ADMIN_USER_IDS = ['TD7UkyNpawhnTZfKunrsbjycqHV2']; 
+export const ADMIN_USER_IDS = ['F0KnzC4xHBRXLCq0OKLjjERGh7l2']; 
 // -------------------------
 
 // Custom error for snippet limit
