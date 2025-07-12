621f0126ce194e5802e8b94b974c3023361ad708
AlphaVerse <eymencoban09@gmail.com>
2025-07-12 15:49:47 +0000

    Change react icon to atom-like icon. Change "C#" icon to C#, change java

--- src/components/layout/main-category-nav.tsx
+++ src/components/layout/main-category-nav.tsx
@@ -3,38 +3,38 @@
 
 import type { SnippetCategory } from "@/types";
 import { SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "@/components/ui/sidebar";
-import { ListFilter, CodeSquare, Palette, FileCode2, FileQuestion, Database, FileText, LayoutGrid, Type, Code, ChevronDown, ChevronUp } from "lucide-react";
+import { ListFilter, CodeSquare, Palette, FileCode2, FileQuestion, Database, FileText, LayoutGrid, Type, Code, ChevronDown, ChevronUp, Atom, Coffee } from "lucide-react";
 import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";
 
-interface MainCategoryNavProps {
-  activeMainCategory: string; 
-  onSelectMainCategory: (category: string) => void;
-}
-
 const JavaIcon = () => (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12.333 13.917c.5.583 1.083 1 1.75 1.25s1.333.333 2 .167c.667-.167 1.25-.5 1.75-1s.917-1.083 1.25-1.75c-1.5-.167-2.917-.083-4.25.25-1.083.25-2.083.667-3 1.25V18.5c1.417-1 2.917-1.583 4.5-1.75.167-.833.333-1.667.5-2.5-.5-.417-1-.75-1.5-1zM5 13.75c1.25-1.083 2.583-1.75 4-2 .167-.833.333-1.667.5-2.5-1.5.333-2.917.917-4.25 1.75C5.083 12.167 5 13 5 13.75zm13.167-2.5c.083-.667.083-1.333 0-2-.083-.667-.25-1.25-.5-1.75s-.583-.917-1-1.25-1-.5-1.5-.5-1 .167-1.5.5c-.5.333-.917.75-1.25 1.25.5 1 1.167 1.833 2 2.5.833.667 1.75 1.083 2.75 1.25zM4 9.5a2.5 2.5 0 0 1 1.75-2.417A4.25 4.25 0 0 1 8.5 5.5c1.167 0 2.167.333 3 .917.833.583 1.417 1.333 1.75 2.25.333.917.417 1.833.25 2.75A12.8 12.8 0 0 1 9.5 13c-.833-1-1.417-2.083-1.75-3.25A2.73 2.73 0 0 1 7 9.5c-.333 0-.667-.083-1-.25S5.5 8.75 5.25 8.5C4.583 9 4.167 9.25 4 9.5z" fill="currentColor"/>
-    </svg>
+    <Coffee />
 );
 
 const GoIcon = () => (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12.5 12.25h-1.5v-2h-2v-1.5h2v-2h1.5v2h2v1.5h-2v2zm3.083-4.5a3.8 3.8 0 0 1-1.083.833A4.25 4.25 0 0 1 12 9c-1.167 0-2.167-.333-3-.917-.833-.583-1.5-1.417-2-2.5S6.583 3.5 7.167 2.833a4.08 4.08 0 0 1 3.5-1.166 4.3 4.3 0 0 1 3.083 1.833c.5.833.75 1.75.75 2.75 0 .833-.167 1.583-.5 2.25zM17 17.5a4.239 4.239 0 0 1-4.25 4.25H9.5a5.25 5.25 0 0 1-4-1.75 4.083 4.083 0 0 1-1.5-3.5v-5.5h1.5v5.5c0 1 .333 1.833 1 2.5.667.667 1.417 1 2.25 1H12a2.75 2.75 0 0 0 2.75-2.75V15h1.5v2.5z" fill="currentColor"/>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4">
+        <text x="2" y="18" fontFamily="monospace" fontSize="18" fill="currentColor">GO</text>
     </svg>
 );
 
 const PHPIcon = () => (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M8.5 15.5a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7zm7.5-3.5a3.5 3.5 0 1 0-7 0h7zM12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2z" fill="currentColor"/>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4">
+        <text x="0" y="18" fontFamily="monospace" fontSize="16" fill="currentColor">PHP</text>
     </svg>
 );
 
 const CppIcon = () => (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M14.5 14.5h-1.5V16h-2v-1.5H9.5v-2h1.5V11h2v1.5h1.5v2zm3 0h-1.5V16h-2v-1.5H15v-2h1.5V11h2v1.5H20v2zM9.5 4A4.5 4.5 0 0 0 5 8.5v7A4.5 4.5 0 0 0 9.5 20h10a4.5 4.5 0 0 0 4.5-4.5v-7A4.5 4.5 0 0 0 19.5 4h-10zM8 8.5a1.5 1.5 0 0 1 1.5-1.5h10a1.5 1.5 0 0 1 1.5 1.5v7a1.5 1.5 0 0 1-1.5 1.5h-10A1.5 1.5 0 0 1 8 15.5v-7z" fill="currentColor"/>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4">
+       <text x="2" y="18" fontFamily="monospace" fontSize="18" fill="currentColor">C++</text>
     </svg>
 );
+
+const CSharpIcon = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4">
+        <text x="4" y="18" fontFamily="monospace" fontSize="18" fill="currentColor">C#</text>
+    </svg>
+);
+
 
 const KotlinIcon = () => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -43,8 +43,8 @@ const KotlinIcon = () => (
 );
 
 const RustIcon = () => (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M21.5 13.25v-2.5h-4.25a.76.76 0 0 0-.75.75v1a.76.76 0 0 0 .75.75h4.25zM17.25 9.5V7H22v2.5h-4.75zM16 19a1 1 0 0 1-.75-1.625L17.5 15h-2.25a1.75 1.75 0 0 1-1.75-1.75v-2.5a1.75 1.75 0 0 1 1.75-1.75h5.5v-1.5H2.5v1.5h6.25v8.25L2.5 19v1.5h20V19h-6.5zM11.75 17V8.75h-2.5V17h2.5z" fill="currentColor"/>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4">
+        <text x="6" y="18" fontFamily="monospace" fontSize="18" fill="currentColor">R</text>
     </svg>
 );
 
@@ -71,10 +71,10 @@ const mainCategoriesList: Array<{ name: SnippetCategory | "All"; icon: React.Ele
   { name: "HTML", icon: CodeSquare },
   { name: "CSS", icon: Palette },
   { name: "JavaScript", icon: FileCode2 },
-  { name: "React", icon: LayoutGrid },
+  { name: "React", icon: Atom },
   { name: "TypeScript", icon: Type },
   { name: "Python", icon: FileText },
-  { name: "C#", icon: Code },
+  { name: "C#", icon: CSharpIcon },
   { name: "SQL", icon: Database },
   { name: "Java", icon: JavaIcon },
   { name: "Go", icon: GoIcon },
